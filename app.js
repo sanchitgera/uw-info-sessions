59d829e0fc6a0af5961f97092cab35f98112ca5a
Sanchit Gera <gerasanchit@gmail.com>
2016-02-28 23:37:56 -0500

    remove dependncy

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var uwapi = require('uwapi')(apiKey);
 var path = require('path');
 var icalendar = require('icalendar');
 var fs = require('fs');
-var api = require('uwaterloo-api');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
